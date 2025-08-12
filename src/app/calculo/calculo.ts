b94230d914848e306398a4ffb4d854b59d33e2af
MariaMariEdu <Maric.brito15@gmail.com>
2025-08-12 15:27:33 -0300

    Fim da revisão

--- src/app/calculo/calculo.ts
+++ src/app/calculo/calculo.ts
@@ -10,7 +10,7 @@ export class Calculo {
   weight: number =0;
   height: number =0;
   bmi: number =0;
-img: string = '../assets/img/imcNull.jpeg';
+img: string = '../assets/abaixo.jpeg';
   class: string = "(￣︶￣)↗ Insira os dados nos campos:"
 
   operationBmi(){
@@ -18,7 +18,7 @@ if (this.height <= 0|| this.weight <= 0)
   {
   this.bmi =0;
   this.class ="insira sua altura"
-  this.img = '../assets/img/imcNull.jpeg'
+  this.img = '../assets/abaixo.jpeg'
   return;
 }  
 
@@ -26,27 +26,27 @@ this.bmi = this.weight / (this.height * this.height);
 
 if (this.bmi >=16){
   this.class = '＞﹏＜ Muito abaixo do peso!';
-  this.img = '../assets/img/muitoAbaixo.jpeg';
+  this.img = '../assets/muitoAbaixo.jpeg';
 }
 else if (this.bmi >17 && this.bmi <18.5){
   this.class = '＞︿＜ Abaixo do peso';
-  this.img = '../assets/img/abaixo.jpeg';
+  this.img = '../assets/abaixo.jpeg';
 }
 else if (this.bmi >18.5 && this.bmi <24.9){
   this.class = '\^o^/ Saudavel!';
-  this.img = '../assets/img/normativo.jpeg';
+  this.img = '../assets/normativo.jpeg';
 }
 else if (this.bmi >25 && this.bmi <29.9){
   this.class = '╯︿╰ Acima do peso...';
-  this.img = '../assets/img/acima.jpeg';
+  this.img = '../assets/acima.jpeg';
 }
  else if (this.bmi >30 && this.bmi < 34.9){
   this.class = '≧ ﹏ ≦ Obesidade grau I';
-  this.img = '../assets/img/obesidade1';
+  this.img = '../assets/obesidade1.jpeg';
 }
 else if (this.bmi >=35 && 40){
   this.class = 'ಥ_ಥ Obesidade grau II';
-  this.img = '../assets/img/acima.jpeg';
+  this.img = '../assets/acima.jpeg';
 }
 }
 }
