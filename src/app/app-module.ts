67c524d533694820d6c541f5ead89d7217e93c5a
MariaMariEdu <maric.brito15@gmail.com>
2025-08-12 10:28:21 -0300

    Declaração das variaveis,  criação da operação logica, realização do import forms module

---
+++ src/app/app-module.ts
@@ -0,0 +1,26 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Calculo } from './calculo/calculo';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    App,
+    Calculo
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    FormsModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
---
+++ src/app/calculo/calculo.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculo',
+  standalone: false,
+  templateUrl: './calculo.html',
+  styleUrl: './calculo.css'
+})
+export class Calculo {
+  weight: number =0;
+  height: number =0;
+  bmi: number =0;
+img: string = '../assets/img/imcNull.png';
+  class: string = "(￣︶￣)↗ Insira os dados nos campos abaixo:"
+
+  operationBmi(){
+if (this.height <= 0|| this.weight <= 0) 
+  {
+  this.bmi =0;
+  this.class ="insira sua altura"
+  this.img = '../assets/img/imcNull.png'
+  return;
+}  
+
+this.bmi = this.weight / (this.height * this.height);
+
+if (this.bmi <16){
+  this.class = '＞﹏＜ Muito abaixo do peso!';
+  this.img = '../assets/img/muitoAbaixo.jpeg';
+}
+else if (this.bmi >17 && this.bmi <18.5){
+  this.class = '＞︿＜ Abaixo do peso';
+  this.img = '../assets/img/abaixo.jpeg';
+}
+else if (this.bmi >18.5 && this.bmi <24.9){
+  this.class = '\^o^/ Saudavel!';
+  this.img = '../assets/img/normativo.jpeg';
+}
+else if (this.bmi >25 && this.bmi <29.9){
+  this.class = '╯︿╰ Acima do peso...';
+  this.img = '../assets/img/acima.jpeg';
+}
+ else if (this.bmi > 30 && this.bmi < 34.9){
+  this.class = '≧ ﹏ ≦ Obesidade grau I';
+  this.img = '../assets/img/obesidade1';
+}
+else if (this.bmi > 35 && 40){
+  this.class = 'ಥ_ಥ Obesidade grau II';
+  this.img = '../assets/img/acima.jpeg';
+}
+}
+}
